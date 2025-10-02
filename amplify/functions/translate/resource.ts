be465a357aadbeeedbdc59f7040fa97ee43af90f
subhayum <musbhayu@amazon.com>
2025-10-02 22:18:44 +0530

    lambda-update

--- amplify/functions/translate/resource.ts
+++ amplify/functions/translate/resource.ts
@@ -3,7 +3,9 @@ import { defineFunction } from '@aws-amplify/backend';
 export const translateFunction = defineFunction({
   name: 'translate',
   entry: './handler.ts',
+  runtime: 20,
+  timeout: 30,
   environment: {
     // Add any environment variables if needed
-  },
+  }
 });
